0aa618e01fd4c890dc2b1573f54660857c76823d
insanity <bokuhe@gmail.com>
2022-07-19 09:34:41 +0900

    fix: react-native prettier url

--- rn/rn-conf.sh
+++ rn/rn-conf.sh
@@ -5,7 +5,7 @@ curl -o .eslintignore https://raw.githubusercontent.com/bokuhe/rc/main/rn/.eslin
 curl -o .eslintrc.js https://raw.githubusercontent.com/bokuhe/rc/main/rn/.eslintrc.js
 
 # PRETTIER
-curl -o .prettierignore https://raw.githubusercontent.com/bokuhe/rc/main/rn/.prettierignore.js
+curl -o .prettierignore https://raw.githubusercontent.com/bokuhe/rc/main/rn/.prettierignore
 curl -o .prettierrc.js https://raw.githubusercontent.com/bokuhe/rc/main/rn/.prettierrc.js
 
 # GITIGNORE
